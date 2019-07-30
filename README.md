ab08e3c7b13e6b66de24d78e249ef71bb6a8c710
NicEastvillage <nicoesterby@gmail.com>
2019-07-30 15:18:19 +0200

    Renamed to Imp the Impaler.

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-# RLBotPythonExample
-Example of a python bot using the RLBot framework
+# Imp the Impaler
+Devilish and simple Spike Rush bot.
 
-## Installation
+This bot is a Rocket League bot made with [RLBot](rlbot.org) for offline play. Was created for the Spikeborne tournament.
 
-### Video guide
-
-https://www.youtube.com/watch?v=UjsQFNN0nSA
-
-### Plain instructions
+### How to use
 
 1. Make sure you've installed [Python 3.7 64 bit](https://www.python.org/downloads/). During installation:
    - Select "Add Python to PATH"
@@ -15,10 +11,3 @@ https://www.youtube.com/watch?v=UjsQFNN0nSA
 1. Download or clone this repository
 1. In the files from the previous step, find and double click on run-gui.bat
 1. Click the 'Run' button
-
-## Changing the bot
-
-- Bot behavior is controlled by `python_example/python_example.py`
-- Bot appearance is controlled by `python_example/appearance.cfg`
-
-See https://github.com/RLBot/RLBotPythonExample/wiki for documentation and tutorials.
--- impaler/impaler.py
+++ impaler/impaler.py
@@ -7,7 +7,7 @@ from util.orientation import Orientation
 from util.vec import Vec3
 
 
-class PythonExample(BaseAgent):
+class ImpalerBot(BaseAgent):
 
     def initialize_agent(self):
         # This runs once before the bot starts up
@@ -30,15 +30,11 @@ class PythonExample(BaseAgent):
         if steer_correction_radians > 0:
             # Positive radians in the unit circle is a turn to the left.
             turn = -1.0  # Negative value for a turn to the left.
-            action_display = "turn left"
         else:
             turn = 1.0
-            action_display = "turn right"
 
         self.controller_state.throttle = 1.0
         self.controller_state.steer = turn
-
-        draw_debug(self.renderer, my_car, packet.game_ball, action_display)
 
         return self.controller_state
 
@@ -60,12 +56,3 @@ def find_correction(current: Vec3, ideal: Vec3) -> float:
             diff -= 2 * math.pi
 
     return diff
-
-
-def draw_debug(renderer, car, ball, action_display):
-    renderer.begin_rendering()
-    # draw a line from the car to the ball
-    renderer.draw_line_3d(car.physics.location, ball.physics.location, renderer.white())
-    # print the action that the bot is taking
-    renderer.draw_string_3d(car.physics.location, 2, 2, action_display, renderer.white())
-    renderer.end_rendering()
